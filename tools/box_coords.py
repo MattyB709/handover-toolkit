7c568ef853aa777f4f8d6359f64e105e5c122087
mattyb709 <matthew.builes8@gmail.com>
2026-03-08 14:49:52 -0500

    got all april tag coordinates done, finally

---
+++ tools/box_coords.py
@@ -0,0 +1,125 @@
+import numpy as np
+import cv2
+
+# mm
+BOX_SIZE_X = 190 
+BOX_SIZE_Y = 270
+BOX_SIZE_Z = 64
+TAG_SIZE = 44 # size of just the black box of the tag
+TAG_WHITE_SIZE = 7 # size of the whites, the total tag length is TAG_SIZE + 2 * TAG_WHITE_SIZE
+
+# 6 DOF pose of the box in the camera frame
+# TODO this goes clockwise from top left, 
+# need to make sure it aligns with detected april tag order
+tag_0_coords = np.array([[(BOX_SIZE_X / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE, 0], 
+                         [(BOX_SIZE_X / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE, 0],
+                         [(BOX_SIZE_X / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE - TAG_SIZE, 0],
+                         [(BOX_SIZE_X / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE - TAG_SIZE, 0]
+                         ])
+
+tag_1_coords = np.array([[(BOX_SIZE_X * 3 / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE, 0],
+                         [(BOX_SIZE_X * 3 / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE, 0],
+                         [(BOX_SIZE_X * 3 / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE - TAG_SIZE, 0],
+                         [(BOX_SIZE_X * 3 / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE - TAG_SIZE, 0]
+                         ])
+
+tag_2_coords = np.array([[(BOX_SIZE_X / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + (TAG_WHITE_SIZE + TAG_SIZE), 0],
+                         [(BOX_SIZE_X / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + (TAG_WHITE_SIZE + TAG_SIZE), 0],
+                         [(BOX_SIZE_X / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + TAG_WHITE_SIZE ,0],
+                         [(BOX_SIZE_X / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + TAG_WHITE_SIZE , 0]
+                         ])
+
+tag_3_coords = np.array([[(BOX_SIZE_X * 3 / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + (TAG_WHITE_SIZE + TAG_SIZE), 0],
+                         [(BOX_SIZE_X * 3 / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + (TAG_WHITE_SIZE + TAG_SIZE), 0],
+                         [(BOX_SIZE_X * 3 / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + TAG_WHITE_SIZE, 0],
+                         [(BOX_SIZE_X * 3 / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + TAG_WHITE_SIZE, 0]
+                         ])
+
+tag_4_coords = np.array([[(BOX_SIZE_X * 3 / 4) + (TAG_SIZE / 2), BOX_SIZE_Y, (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [(BOX_SIZE_X * 3 / 4) - (TAG_SIZE / 2), BOX_SIZE_Y, (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [(BOX_SIZE_X * 3 / 4) - (TAG_SIZE / 2), BOX_SIZE_Y, (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)],
+                         [(BOX_SIZE_X * 3 / 4) + (TAG_SIZE / 2), BOX_SIZE_Y, (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)]
+                         ])
+
+tag_5_coords = np.array([[(BOX_SIZE_X / 4) + (TAG_SIZE / 2), BOX_SIZE_Y, (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [(BOX_SIZE_X / 4) - (TAG_SIZE / 2), BOX_SIZE_Y, (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [(BOX_SIZE_X / 4) - (TAG_SIZE / 2), BOX_SIZE_Y, (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)],
+                         [(BOX_SIZE_X / 4) + (TAG_SIZE / 2), BOX_SIZE_Y, (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)]
+                         ])
+                
+tag_6_coords = np.array([[0, (BOX_SIZE_Y * 3 / 4) + (TAG_SIZE / 2), (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [0, (BOX_SIZE_Y * 3 / 4) - (TAG_SIZE / 2), (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [0, (BOX_SIZE_Y * 3 / 4) - (TAG_SIZE / 2), (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)],
+                         [0, (BOX_SIZE_Y * 3 / 4) + (TAG_SIZE / 2), (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)]
+                         ])
+                    
+tag_7_coords = np.array([[0, (BOX_SIZE_Y / 2) + (TAG_SIZE / 2), (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [0, (BOX_SIZE_Y / 2) - (TAG_SIZE / 2), (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [0, (BOX_SIZE_Y / 2) - (TAG_SIZE / 2), (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)],
+                         [0, (BOX_SIZE_Y / 2) + (TAG_SIZE / 2), (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)]
+                         ])
+
+tag_8_coords = np.array([[0, (BOX_SIZE_Y / 4) + (TAG_SIZE / 2), (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [0, (BOX_SIZE_Y / 4) - (TAG_SIZE / 2), (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [0, (BOX_SIZE_Y / 4) - (TAG_SIZE / 2), (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)],
+                         [0, (BOX_SIZE_Y / 4) + (TAG_SIZE / 2), (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)]
+                         ])
+
+tag_9_coords = np.array([[(BOX_SIZE_X / 4) - (TAG_SIZE / 2), 0, (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [(BOX_SIZE_X / 4) + (TAG_SIZE / 2), 0, (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [(BOX_SIZE_X / 4) + (TAG_SIZE / 2), 0, (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)],
+                         [(BOX_SIZE_X / 4) - (TAG_SIZE / 2), 0, (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)]
+                         ])
+
+tag_10_coords = np.array([[(BOX_SIZE_X * 3 / 4) - (TAG_SIZE / 2), 0, (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [(BOX_SIZE_X * 3 / 4) + (TAG_SIZE / 2), 0, (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [(BOX_SIZE_X * 3 / 4) + (TAG_SIZE / 2), 0, (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)],
+                         [(BOX_SIZE_X * 3 / 4) - (TAG_SIZE / 2), 0, (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)]
+                         ])
+
+tag_11_coords = np.array([[BOX_SIZE_X, (BOX_SIZE_Y / 4) - (TAG_SIZE / 2), (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [BOX_SIZE_X, (BOX_SIZE_Y / 4) + (TAG_SIZE / 2), (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [BOX_SIZE_X, (BOX_SIZE_Y / 4) + (TAG_SIZE / 2), (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)],
+                         [BOX_SIZE_X, (BOX_SIZE_Y / 4) - (TAG_SIZE / 2), (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)]
+                         ])
+
+tag_12_coords = np.array([[BOX_SIZE_X, (BOX_SIZE_Y / 2) - (TAG_SIZE / 2), (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [BOX_SIZE_X, (BOX_SIZE_Y / 2) + (TAG_SIZE / 2), (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [BOX_SIZE_X, (BOX_SIZE_Y / 2) + (TAG_SIZE / 2), (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)],
+                         [BOX_SIZE_X, (BOX_SIZE_Y / 2) - (TAG_SIZE / 2), (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)]
+                         ])
+
+tag_13_coords = np.array([[BOX_SIZE_X, (BOX_SIZE_Y * 3/ 4) - (TAG_SIZE / 2), (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [BOX_SIZE_X, (BOX_SIZE_Y * 3 / 4) + (TAG_SIZE / 2), (BOX_SIZE_Z / 2) - (TAG_SIZE / 2)],
+                         [BOX_SIZE_X, (BOX_SIZE_Y * 3 / 4) + (TAG_SIZE / 2), (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)],
+                         [BOX_SIZE_X, (BOX_SIZE_Y * 3 / 4) - (TAG_SIZE / 2), (BOX_SIZE_Z / 2) + (TAG_SIZE / 2)]
+                         ])
+                        
+tag_14_coords = np.array([[(BOX_SIZE_X / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + TAG_WHITE_SIZE, BOX_SIZE_Z],
+                         [(BOX_SIZE_X / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + TAG_WHITE_SIZE, BOX_SIZE_Z],
+                         [(BOX_SIZE_X / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + TAG_WHITE_SIZE + TAG_SIZE, BOX_SIZE_Z],
+                         [(BOX_SIZE_X / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + TAG_WHITE_SIZE + TAG_SIZE, BOX_SIZE_Z]
+                         ])
+
+tag_15_coords = np.array([[(BOX_SIZE_X * 3 / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + TAG_WHITE_SIZE, BOX_SIZE_Z],
+                         [(BOX_SIZE_X * 3 / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + TAG_WHITE_SIZE, BOX_SIZE_Z],
+                         [(BOX_SIZE_X * 3 / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + TAG_WHITE_SIZE + TAG_SIZE, BOX_SIZE_Z],
+                         [(BOX_SIZE_X * 3 / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y / 4) + TAG_WHITE_SIZE + TAG_SIZE, BOX_SIZE_Z],
+                         ])
+
+tag_16_coords = np.array([[(BOX_SIZE_X / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE - TAG_SIZE, BOX_SIZE_Z],
+                         [(BOX_SIZE_X / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE - TAG_SIZE, BOX_SIZE_Z],
+                         [(BOX_SIZE_X / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE, BOX_SIZE_Z],
+                         [(BOX_SIZE_X / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE, BOX_SIZE_Z],
+                         ])
+
+tag_17_coords = np.array([[(BOX_SIZE_X * 3 / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE - TAG_SIZE, BOX_SIZE_Z],
+                         [(BOX_SIZE_X * 3 / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE - TAG_SIZE, BOX_SIZE_Z],
+                         [(BOX_SIZE_X * 3 / 4) + (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE, BOX_SIZE_Z],
+                         [(BOX_SIZE_X * 3 / 4) - (TAG_SIZE / 2), (BOX_SIZE_Y * 3 / 4) - TAG_WHITE_SIZE, BOX_SIZE_Z],
+                         ])
+
+all_tag_coords = np.concatenate([tag_0_coords, tag_1_coords, tag_2_coords, tag_3_coords, tag_4_coords,
+                                  tag_5_coords, tag_6_coords, tag_7_coords, tag_8_coords, tag_9_coords,
+                                  tag_10_coords, tag_11_coords, tag_12_coords, tag_13_coords, tag_14_coords,
+                                  tag_15_coords, tag_16_coords, tag_17_coords])
